6dc4e6e59c6f72bcf9733bfcb7a72e6091f4330f
FarabyMalau <farabymuzakki49@gmail.com>
2025-06-26 18:46:46 +0600

    selesai

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -60,7 +60,11 @@ public:
         // Step 6: if the value in the data field of the new node is less than that of parent
         if (x < parent->info)
         {
-            
+            // 6a: Make the left child of parent point to the new node
+            parent->leftchild = newNode;
+
+            // 6b: Exit
+            return;
         }
 
     }
